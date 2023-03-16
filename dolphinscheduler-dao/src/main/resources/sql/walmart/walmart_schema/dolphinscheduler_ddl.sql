7343b6903f3189b2591f367c802272e780739fb3
Liz <leezee0723@163.com>
2023-03-16 16:11:58 +0800

    [fix]: 202303161610 change sql table name t_ds_alert_group to t_ds_alertgroup;

--- dolphinscheduler-dao/src/main/resources/sql/walmart/walmart_schema/dolphinscheduler_ddl.sql
+++ dolphinscheduler-dao/src/main/resources/sql/walmart/walmart_schema/dolphinscheduler_ddl.sql
@@ -1,3 +1,3 @@
 alter table t_ds_process_definition drop receivers;
 alter table t_ds_process_definition drop receivers_cc;
-alter table t_ds_alert_group drop group_type;
+alter table t_ds_alertgroup drop group_type;
